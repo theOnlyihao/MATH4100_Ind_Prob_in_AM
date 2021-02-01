042782820384196474aa5914d04991dcef48b3d0
theOnlyi <62514637+theOnlyi@users.noreply.github.com>
2021-02-01 09:43:14 -0500

    Add files via upload

---
+++ Week 1 Statistical Analysis for the Datasets.R
@@ -0,0 +1,20 @@
+####### MATH4100 Week 1: Statistical Analysis ####### 
+setwd("/Users/yihao/Desktop/Yihao/Wentworth/Mathmatics /MATH4100 Industrial Prob in applied math/nurse_and_behavior_data")
+nurse_info<-read.csv("nurse_info.csv", header = TRUE)
+accept_behaivor<-read.csv("accept_behavior.csv", header = TRUE)
+app_behavior<-read.csv("app_behavior.csv", header = TRUE)
+accept_behaivor<-accept_behaivor[,c(-2,-4,-5)]
+accept_behaivor<-accept_behaivor[1:200,] #we took first 200 observations to pratice the method we learned. 
+plot(accept_behaivor$pid~accept_behaivor$shift_time)
+typeof(accept_behaivor$shift_time)
+dim(accept_behaivor)
+shift_time<-accept_behaivor$shift_time
+#accept_behaivor<-data.frame(accept_behaivor, date=as.Date.POSIXlt(shift_time,date="%Y-%m-%d %H:%M:%S"))
+#strptime(shift_time,"%Y-%m-%d %H:%M:%S", tz="EST")
+accept_behaivor<-data.frame(accept_behaivor,shift_time=strptime(shift_time,"%Y-%m-%d %H:%M:%S", tz="EST"))
+accept_behaivor<-accept_behaivor[,c(-2)]
+dim(accept_behaivor)
+accept_behaivor<-as.matrix(accept_behaivor)
+?kmeans
+kmeans(accept_behaivor,3)
+plo
