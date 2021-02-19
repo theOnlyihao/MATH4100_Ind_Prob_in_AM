2238c2d12491d403af9b9fbe09b815cc9ba5fc6e
theOnlyi <62514637+theOnlyi@users.noreply.github.com>
2021-02-18 20:23:20 -0500

    Update accept_behavior

--- MATH4100.R
+++ MATH4100.R
@@ -1,28 +1,44 @@
 ####### MATH4100 Week 1: Statistical Analysis ####### 
-##### library we used
-####### MATH4100 Week 1: Statistical Analysis ####### 
-##### library we used
+###### library ###### 
 library(timeDate)
 library(dplyr)
 library(ggplot2)
+library(batman)
 
-###### data cleaning 
+###### data cleaning ###### 
 setwd("...")
 nurse_info<-read.csv("nurse_info.csv", header = TRUE)
 accept_behaivor<-read.csv("accept_behavior.csv", header = TRUE)
 app_behavior<-read.csv("app_behavior.csv", header = TRUE)
-accept_behaivor<-data.frame(accept_behaivor,shift_date=as.Date(accept_behaivor$shift_time,"%Y-%m-%d"))
-accept_behaivor<-accept_behaivor[,c(-3)]
 
 ########### accept behavior ##########
-day<-accept_behaivor$shift_date
-gmt_shift_time<-timeDate(day, FinCenter = "GMT")
-day<-dayOfWeek(gmt_shift_time)
-accept_behaivor<-data.frame(accept_behaivor,day)
-head(accept_behaivor)
-weekend<-isWeekend(gmt_shift_time)
-accept_behaivor<-data.frame(accept_behaivor,weekend)
-head(accept_behaivor)
+accept_behaivor<-data.frame(accept_behaivor,accept_date=as.Date(accept_behaivor$accept_time,"%Y-%m-%d"))
+accept_behaivor<-data.frame(accept_behaivor,shift_date=as.Date(accept_behaivor$shift_time,"%Y-%m-%d"))
+accept_behaivor<-data.frame(accept_behaivor,release_date=as.Date(accept_behaivor$release_time,"%Y-%m-%d"))
+#accept_behaivor<-accept_behaivor[,c(-3)]
+gmt_accept_time<-timeDate(accept_behaivor$accept_date,FinCenter = "GMT")
+gmt_shift_time<-timeDate(accept_behaivor$shift_date, FinCenter = "GMT")
+gmt_release_time<-timeDate(accept_behaivor$release_date,FinCenter = "GMT")
+accept_day<-dayOfWeek(gmt_accept_time)
+shift_day<-dayOfWeek(gmt_shift_time)
+release_day<-dayOfWeek(gmt_release_time)
+accept_behaivor<-data.frame(accept_behaivor,accept_day,shift_day,release_day)
+accept_behaivor<-accept_behaivor[,c(-2,-3,-4,-5)]
+accept_behaivor<-accept_behaivor[,c(1,2,5,3,6,4,7)]
+weekend_shift<-isWeekend(gmt_shift_time)
+weekend_release<-isWeekend(gmt_release_time)
+accept_behaivor<-data.frame(accept_behaivor,weekend_shift,weekend_release)
+accept_behaivor<-accept_behaivor[,c(-9)]
+
+
+
+weekend_shift<-subset(accept_behaivor,accept_behaivor$release_day==c("Sat","Sun"))
+release_shift<-subset(accept_behaivor,accept_behaivor$release_day==c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))
+release_shift<-data.frame(release_shift)
+accept_behaivor_no_release<-subset(accept_behaivor,is.na(accept_behaivor$release_date))
+
+
+accept_behaivor<-data.frame(accept_behaivor,release_day_isweekend)
 holiday<-isHoliday(gmt_shift_time)
 accept_behaivor<-data.frame(accept_behaivor,holiday)
 isweekend<-(accept_behaivor$weekend=="TRUE")*1
@@ -33,10 +49,6 @@ accept_behaivor<-accept_behaivor[,c(-2,-7,-8)]
 year_month<-format(accept_behaivor$shift_date,"%Y-%m")
 accept_behaivor<-data.frame(accept_behaivor,year_month)
 #holidayNYSE(accept_behaivor$shift_date)
-?listHolidays
-?holidayZURICH
-?holidayNERC
-?holidayNYSE
 
 ########### accept plot ###########
 weekendshift1<-subset(accept_behaivor,accept_behaivor$day=="Sat" | accept_behaivor$day=="Sun",select = c(pid,day,year_month))
